0b6702d897cdbec48723e29980d753570eb5fe48
emo <2381248658@qq.com>
2025-12-08 00:25:02 +0800

    完成中...

---
+++ src/views/Layout/components/LayoutHeader.vue
@@ -0,0 +1,79 @@
+<template>
+  <header class="header-main">
+    <div class="header-cotainer">
+      <h1 class="logo">
+        <a href="/">EasyHub</a>
+      </h1>
+      <ul class="header-title">
+        <li><a href="#">居家</a></li>
+        <li><a href="#">美食</a></li>
+        <li><a href="#">服饰</a></li>
+        <li><a href="#">母婴</a></li>
+        <li><a href="#">个护</a></li>
+        <li><a href="#">严选</a></li>
+        <li><a href="#">数码</a></li>
+        <li><a href="#">运动</a></li>
+        <li><a href="#">杂项</a></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header-main {
+  width: 1240px;
+  height: 132px;
+  margin: 0 auto;
+  // 显示页面区域需要 完成时删去
+  background-color: skyblue;
+}
+
+.header-cotainer {
+  display: flex;
+  align-items: center;
+  /* 确保它撑满了父容器的高度 */
+  height: 100%;
+}
+
+.logo {
+  width: 200px;
+  height: 100%; // 继承 header-main 的高度
+
+  a {
+    display: block;
+    height: 100%;
+    width: 100%;
+
+    text-indent: -9999px;
+    background: url('@/assets/images/logo.png') no-repeat center center / contain;
+  }
+}
+
+.header-title {
+  display: flex;
+  width: 820px;
+  padding-left: 40px;
+
+  li {
+    margin-right: 40px;
+    width: 38px;
+    text-align: center;
+    list-style-type: none;
+
+    a {
+      font-size: 16px;
+      line-height: 32px;
+      height: 32px;
+      display: inline-block;
+      // ⚠️ 补充：移除下划线
+      text-decoration: none;
+      color: #333;
+
+      &:hover {
+        color: $xtxColor;
+        border-bottom: 1px solid $xtxColor;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Layout/components/LayoutNav.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="nav-main">
+    <div class="nav-inner">
+      <a href="#">周杰伦</a>
+      <a href="#">退出登录</a>
+      <a href="#">我的订单</a>
+      <a href="#">会员中心</a>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.nav-main {
+  height: 53px;
+  background-color: #333;
+}
+
+.nav-inner {
+  width: 1240px;
+  margin: 0 auto;
+  height: 100%;
+
+  /* Flex 布局核心配置 */
+  display: flex;
+  /* 1. 子元素靠右对齐 */
+  justify-content: flex-end;
+  /* 2. 子元素垂直居中 */
+  align-items: center;
+
+}
+
+/* 链接样式 */
+.nav-inner a {
+  /* 链接必须是块级或行内块级元素才能设置 padding/margin/border */
+  display: block;
+  /* 行高设置为 1 即可 */
+  line-height: 1;
+  color: #cdcdcd;
+  font-size: 14px;
+  /* 设置左右内边距 */
+  padding: 0 15px;
+  // 设置文字样式
+  text-decoration: none;
+}
+
+/* 创建垂直分割线 (推荐使用 border-left) */
+.nav-inner a:not(:first-child) {
+  /* 或者使用 .nav-inner a ~ a { ... } */
+  border-left: 2px solid #666;
+  /* 只给非第一个链接添加左边框 */
+}
+
+/* 悬停效果 */
+.nav-inner a:hover {
+  color: $xtxColor;
+  /* 悬停时颜色改变 */
+}
+</style>
