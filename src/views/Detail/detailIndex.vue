520184c258599d4d02c91bf887e6fcdf3221f5a5
emo <2381248658@qq.com>
2025-12-13 19:59:28 +0800

    适配主图列表

--- src/views/Detail/detailIndex.vue
+++ src/views/Detail/detailIndex.vue
@@ -3,7 +3,7 @@ import { getDetail } from '@/apis/detail';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import DetailHot from './components/DetailHot.vue';
-import viewIndex from '@/components/imageView/viewIndex.vue';
+import ViewIndex from '@/components/imageView/ViewIndex.vue';
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -38,7 +38,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <viewIndex />
+              <ViewIndex :image-list="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
