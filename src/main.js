fdb32b3a94cedb8c0d0f5af953fe2ce73dce3040
emo <2381248658@qq.com>
2025-12-17 20:29:07 +0800

    修复已知错误

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+// 1. 导入 Pinia 持久化插件
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 // 引入插件
 import imgLazy from './directives/imgLazy'
 import { PluginManager } from './components'
@@ -10,9 +13,17 @@ import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// 2. 创建 Pinia 实例
+const pinia = createPinia()
+
+// 3. 将持久化插件安装到 Pinia 实例中
+pinia.use(piniaPluginPersistedstate)
+
+// 4. 使用 Pinia 实例
+app.use(pinia)
 app.use(router)
-// 使用插件
+
+// 使用其他插件
 app.use(imgLazy)
 app.use(PluginManager)
 
